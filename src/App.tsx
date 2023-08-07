ce3a9bc432aaed7c0bc9aa66a35d9e94df0b410b
lopzz <694293256@qq.com>
2023-08-07 23:34:44 +0800

    feat: button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+// import logo from "./logo.svg";
+// import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        <Button onClick={(e) => alert(12312)}>hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          hello
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          href="http://www.baidu.com"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          baidu link
+        </Button>
       </header>
     </div>
   );
